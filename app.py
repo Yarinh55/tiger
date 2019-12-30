71fb15af8910859a0d0d422a67818e10e8090224
Yarinh55 <yarinh55@gmail.com>
2019-12-30 19:56:05 +0200

    get messages method

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import mysql.connector
+
+mydatabase = mysql.connector.connect(
+    host = 'localhost', user = 'root',
+    passwd = 'LoginPass@@11223344', database = 'tiger')
+
+mycursor = mydatabase.cursor()
 
 app = Flask(__name__)
 
@@ -13,6 +20,16 @@ def Home():
 def contact_us():
     return render_template('/contact_us.html')
 
+@app.route('/messages_view')
+def messages_view():
+    return render_template('/messages_view.html')
+
+@app.route('/messages_view')
+def messages_view():
+	mycursor.execute("SELECT * FROM tiger.messages")
+	data = mycursor.fetchall()
+	return render_template('messages_view.html', data=data)
+	
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
